40973744b877d03d74509d2fc726bd050a11a514
Lisa Lee <snickers495@gmail.com>
2018-03-08 16:20:32 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -12,10 +12,10 @@ class SearchesController < ApplicationController
     end
     body_hash = JSON.parse(@resp.body)
     if @resp.success?
-    @venues = body["response"]["venues"]
-  else
-    @error = body["meta"]["errorDetail"]
-  end
-  render 'search'
+      @venues = body["response"]["venues"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+    render 'search'
   end
 end
